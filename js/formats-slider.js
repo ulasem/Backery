ca4656a425e4bad84622e7545600920c550ec12e
Yuliia <semockoula@gmail.com>
2025-11-02 22:34:20 +0200

    update Bakery

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,8 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
   loop: true,
-
-  slidersPerVeiw: 1,
+  slidesPerView: 1,
   spaceBetween: 20,
   breakpoints: {
     768: {
@@ -12,15 +10,11 @@ const swiper = new Swiper('.swiper-container', {
       slidesPerView: 3,
     },
   },
-
-  // If we need pagination
   pagination: {
     el: '.pagination',
     bulletClass: 'pagination__button',
     bulletActiveClass: 'pagination__button--active',
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.carousel-button.next',
     prevEl: '.carousel-button.prev',
